2d490a6c8b389337ff9bcab93b303d675514b069
Dunham <DUNHAMBR@ad.reyrey.com>
2023-06-13 15:16:04 -0500

    update script.js

--- public/assets/script.js
+++ public/assets/script.js
@@ -5,14 +5,26 @@ const nicknameOne = document.getElementById('nickname-one');
 const nicknameTwo = document.getElementById('nickname-two');
 
 
-// route that gets specific username stored with each browser session
+// route that gets specific username stored with each browser session and passes it to postUserData()
 const getSessName = async () => {
     try {
         const res = await fetch('/getSessName', {
             method: 'GET',
         })
         const data = await res.json()
-        nicknameOne.value = data
+        console.log('LOOKEYHERE')
+        console.log(data)
+        nicknameOne.value = data.nickname
+        const userData = {
+            id: data.id,
+            nickname: data.nickname,
+            loggedIn: true,
+            messages: {
+                sent: [],
+                received: [],
+            }
+        }
+        postUserData(userData)
         return data
     } catch (err) {
         console.log(err)
@@ -51,7 +63,48 @@ const postText = async (userInput) => {
             console.log(err)
         }
     }
-    
+
+}
+
+//takes the new sessName and posts it to chat_db.json
+const postUserData = async (userData) => {
+
+    if (!userData) {
+        console.log('No userData');
+    } else {
+        try {
+            const res = await fetch('/postUserData', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData)
+            })
+            const data = await res.json()
+            console.log('POSTUSERDATA-DATA');
+            console.log(data)
+            //        return data
+        } catch (err) {
+            console.log(err);
+        };
+    };
+};
+
+const postMessageData = async (userInput) => {
+    try {
+        const res = await fetch('/postMessageData', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userInput)
+        })
+        const data = await res.json()
+        console.log('POSTMESSAGEDATA')
+        console.log(data)
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 const submitOneHandler = (e) => {
@@ -62,13 +115,15 @@ const submitOneHandler = (e) => {
         'message': inputOne.value
     }
 
+    //postMessageData(userInput)
+
     postText(userInput)
         .then((newMessage) => {
             inputTwo.value = `${newMessage.nickname} : ${newMessage.message}`
             //nicknameTwo.value = `${newMessage.nickname} : ${newMessage.message}`
         })
         .catch((err) => console.error(err))
-        
+
     inputOne.value = '';
 }
 
